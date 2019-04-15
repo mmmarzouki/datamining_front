5db93fe5003614f8ae52d6c1428e200f6c54833b
mmmarzouki <mmmarzouki@gmail.com>
2019-04-15 19:10:12 +0200

    added calling back service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Movie } from './movie';
+import { MovieService } from './movie.service';
 
 @Component({
   selector: 'app-root',
@@ -15,18 +16,39 @@ export class AppComponent {
   dir_likes = 0;
   cast_likes = 0;
   movie_likes = 0;
-  contentRating =0;
+  contentRating = 0;
+  res = 0;
   movie: Movie ={'data': [0,0,0,0,0,0,0,0,0]}
   title = 'Movie Rater';
 
-  setCountry(x:number) {
-    this.country=x;
+  constructor(private movieService: MovieService){
+
+  }
+
+  setCountry(x:string) {
+    console.log(x);
+    this.country=['USA','UK','Colombia','Other'].indexOf(x);
   }
   
-  setContentRating(x:number) {
-    this.contentRating=x;
+  setContentRating(x:string) {
+    console.log(x);
+    this.contentRating=['R','PG-13','PG','Other'].indexOf(x);
   }
 
-
+  predict() {
+    this.movie.data[0]=this.year;
+    this.movie.data[1]=this.country;
+    this.movie.data[2]=this.duration;
+    this.movie.data[3]=this.contentRating;
+    this.movie.data[4]=this.budget;
+    this.movie.data[5]=this.facenumber;
+    this.movie.data[6]=this.dir_likes;
+    this.movie.data[7]=this.cast_likes;
+    this.movie.data[8]=this.movie_likes;
+    this.movieService.predict(this.movie).subscribe(res=>{
+      console.log(res);
+      this.res = res[0];
+    });
+  }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
